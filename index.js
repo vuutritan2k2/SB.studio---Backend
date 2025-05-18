66083ace6fab9a4e4cd384d5bc0824b4cdd14912
Tri Tan <vuutritan2k2@gmail.com>
2025-05-18 21:21:57 +0700

    03.3 - Fix Cors

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
+  origin: process.env.FRONT_END_URL,
   credentials: true,
-  origin: process.env.FRONT_END_URL
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
